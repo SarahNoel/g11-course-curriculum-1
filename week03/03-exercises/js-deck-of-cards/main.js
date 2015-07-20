32d2920786e8dfbd93ceef383e00ea8a072890dc
omaracrystal <omaracrystal@gmail.com>
2015-07-20 17:52:28 -0600

    Added fuction to create newDeck and to shuffleCards

--- week03/03-exercises/js-deck-of-cards/main.js
+++ week03/03-exercises/js-deck-of-cards/main.js
@@ -20,9 +20,12 @@ function displayCards(){
   }
 }
 
+
 // Creates a deck of 52 cards
 function newDeck(){
-
+  // variables
+  // var ranks = [ "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+  var suits = ["d", "c", "s", "h"];
   var ranks = [
     {card:"a"},
     {card:"2"},
@@ -38,10 +41,36 @@ function newDeck(){
     {card:"q"},
     {card:"k"}
   ];
-
-  var suits = [ "d", "c", "s", "h"];
   var deck = [];
-}
+  // for loops
+  for (i = 0; i < ranks.length; i++) {
+    for (j = 0; j < suits.length; j++) {
+      ranks[i].suit = suits[j];
+      deck.push({
+        card: ranks[i].card, suit: ranks[i].suit});
+    }
+  }
+//have to return new array at the end of a function to be able to run the function
+  return deck;
+};
+
+newDeck();
+
 
 // Shuffles the Deck
-function shuffleCards(cardDeck){}
+// Shuffles the Deck
+function shuffleCards(newDeck){
+    var m = newDeck.length;
+    while (m) {
+      var i = Math.floor(Math.random() * m--);
+      var t = newDeck[m];
+      newDeck[m] = newDeck[i];
+      newDeck[i] = t;
+    }
+    return newDeck;
+};
+
+console.log(displayCards());
+
+
+
